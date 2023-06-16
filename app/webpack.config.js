45589be80faa248b48d5ece31e4bfd819bd15fd1
Jared Anstett <jared.anstett@nokia.com>
2023-06-16 15:11:56 -0400

    first upload

---
+++ app/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: 'development',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/index.html", to: "index.html" }],
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+  },
+};
